7409162a0b6feab51a581cca45b9662261501508
SabrinaGear <sabrinagear.ft@gmail.com>
2019-07-16 05:06:12 -0500

    commit

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,9 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
-
-   return -1   # not found
+      for i in range( 0, len(arr) ):
+          if arr[i] == target:
+              return i
+      return -1   
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -12,10 +12,17 @@ def binary_search(arr, target):
   if len(arr) == 0:
     return -1 # array empty
     
-  low = 0
-  high = len(arr)-1
+  l = 0
+  h = len(arr)-1
 
-  # TO-DO: add missing code
+  while l < h:
+        m = (l+h)/2
+  if target < arr[m]:
+        h = m-1 
+  elif target > arr[m]:
+        l = m+1 
+  else:
+        return m
 
   return -1 # not found
 
